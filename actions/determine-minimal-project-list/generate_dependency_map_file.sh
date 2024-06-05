eac87524fdac8d796c1674c80247427931ad4e85
Zuhayr Khan <ZuhayrKhan@mac.com>
2024-06-05 11:08:51 +0100

    Add new dependencies to Service A and B, update script
    Two new service dependencies were added to Service A and B. Concurrently, modifications are made to the 'generate_dependency_map_file.sh' script to enhance the discovery and handling of module dependencies, specifically for those marked as SNAPSHOT versions. This update should improve dependency tracking within the project.

--- actions/determine-minimal-project-list/generate_dependency_map_file.sh
+++ actions/determine-minimal-project-list/generate_dependency_map_file.sh
@@ -19,14 +19,18 @@ for module in $(find . -name "pom.xml" -exec dirname {} \;); do
   echo "Processing dir: $module"
   cd $module
 
+  unset current_module_GAV
+
   # Use grep to find SNAPSHOT dependencies
   mvn dependency:tree -DoutputType=dot -o | while IFS= read -r line; do
+#    echo "line:$line"
     if [[ $line =~ \"([^\"]+)\"[^\"]*\"([^\"]+)\" ]]; then
       dependent_module=$(trim "${BASH_REMATCH[1]}")
       dependency_module=$(trim "${BASH_REMATCH[2]}")
-      if [[ -v current_module_GAV ]]; then
+#      echo "dependent_module:$dependent_module"
+#      echo "dependency_module:$dependency_module"
+      if [[ ! -z "${current_module_GAV}" ]]; then
         echo "$module/|$current_module_GAV" >> "$maven_map_output_file"
-        unset current_module_GAV
       fi
       if [[ "$dependency_module" == *-SNAPSHOT ]]; then
         echo "$dependent_module|$dependency_module" >> "$dependency_map_output_file"
@@ -35,6 +39,7 @@ for module in $(find . -name "pom.xml" -exec dirname {} \;); do
       current_module_groupId=$(trim "${BASH_REMATCH[1]}")
       current_module_artifactId=$(trim "${BASH_REMATCH[2]}")
       current_module_version=$(trim "${BASH_REMATCH[3]}")
+#      echo "current_module: $current_module_groupId:$current_module_artifactId:$current_module_version"
       if [[ "$module" =~ .*$current_module_artifactId$ ]]; then
         current_module_GAV="$current_module_groupId:$current_module_artifactId:$current_module_version"
       fi
