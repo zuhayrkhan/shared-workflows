8521908c4ffd038bb47bb2a17389ce41255a879f
Zuhayr Khan <ZuhayrKhan@mac.com>
2024-06-29 12:38:02 +0100

    Update dependency mapping in create_project_list.sh
    This commit includes changes to enhance tracking of dependencies and dependents in the create_project_list.sh script. It introduces major improvements that clarify variable naming and track dependency maps more effectively. Also, a new file do_create_project_list.sh is added to source the updated script.

--- actions/determine-minimal-project-list/create_project_list.sh
+++ actions/determine-minimal-project-list/create_project_list.sh
@@ -12,6 +12,7 @@ declare -gA maven_to_folder_map
 declare -gA folder_to_maven_map
 declare -gA affected_modules_map
 declare -gA project_list
+declare -gA dependency_map
 
 process_module_and_dir(){
 
@@ -20,33 +21,67 @@ process_module_and_dir(){
     IFS="(" read -r mavenGAV dir <<< "$mavenGAV_and_dir"
     folder=${dir%)*}
 
-    mavenGAV_escaped=$(escape "$mavenGAV" )
-    folder_escaped=$(escape "$folder" )
-    maven_to_folder_map["$mavenGAV_escaped"]="$folder_escaped "
-    folder_to_maven_map["$folder_escaped"]="$mavenGAV_escaped "
+    dependency_mavenGAV_escaped=$(escape "$mavenGAV" )
+    dependency_folder_escaped=$(escape "$folder" )
+    maven_to_folder_map["$dependency_mavenGAV_escaped"]="$dependency_folder_escaped "
+    folder_to_maven_map["$dependency_folder_escaped"]="$dependency_mavenGAV_escaped "
 
-    echo "$mavenGAV_escaped" "$folder_escaped"
+    echo "$dependency_mavenGAV_escaped" "$dependency_folder_escaped"
 
 }
 
 generate_and_handle_dependency_map() {
 
     while IFS= read -r line; do
+
         IFS="|" read -r dependent dependency <<< "$line"
 
-        read -r mavenGAV_escaped folder_escaped <<< "$(process_module_and_dir "$dependent")"
-        dependent_module_escaped=$mavenGAV_escaped
+        read -r dependent_mavenGAV_escaped dependent_folder_escaped <<< "$(process_module_and_dir "$dependent")"
 
-        maven_to_folder_map["$mavenGAV_escaped"]="$folder_escaped "
-        folder_to_maven_map["$folder_escaped"]="$mavenGAV_escaped "
+        maven_to_folder_map["$dependent_mavenGAV_escaped"]="$dependent_folder_escaped "
+        folder_to_maven_map["$dependent_folder_escaped"]="$dependent_mavenGAV_escaped "
 
-        read -r mavenGAV_escaped folder_escaped <<< "$(process_module_and_dir "$dependency")"
-        dependency_module_escaped=mavenGAV_escaped
+        read -r dependency_mavenGAV_escaped dependency_folder_escaped <<< "$(process_module_and_dir "$dependency")"
 
-        maven_to_folder_map["$mavenGAV_escaped"]="$folder_escaped "
-        folder_to_maven_map["$folder_escaped"]="$mavenGAV_escaped "
+        maven_to_folder_map["$dependency_mavenGAV_escaped"]="$dependency_folder_escaped "
+        folder_to_maven_map["$dependency_folder_escaped"]="$dependency_mavenGAV_escaped "
 
-        dependency_map["$dependency_module_escaped"]+="$dependent_module_escaped "
+        echo "dependency_mavenGAV=$(unescape $dependency_mavenGAV_escaped)"
+        echo "dependent_mavenGAV=$(unescape $dependent_mavenGAV_escaped)"
+        echo "dependent_mavenGAV_escaped=$dependency_mavenGAV_escaped"
+
+        key="nothing"
+        if [[ -v dependency_map["$key"] ]]; then
+          echo "(1a)dependency_map[nothing]=${dependency_map["$key"]}"
+        fi
+
+        if [[ -v dependency_map["$key"] && ${dependency_map["$key"]} ]]; then
+          echo "(2a)dependency_map[nothing]=${dependency_map["$key"]}"
+        fi
+
+        dependency_map["$dependency_mavenGAV_escaped"]+="$dependent_mavenGAV_escaped "
+
+        echo "dependency_map[$(unescape $dependent_mavenGAV_escaped)]=${dependency_map[$dependent_mavenGAV_escaped]}"
+
+        if [[ -v dependency_map["$dependent_mavenGAV_escaped"] ]]; then
+          echo "(0b)dependency_map[$dependent_mavenGAV_escaped]=${dependency_map["$dependent_mavenGAV_escaped"]}"
+        fi
+
+        key="nothing"
+        if [[ -v dependency_map["$key"] ]]; then
+          echo "(1b)dependency_map[nothing]=${dependency_map["$key"]}"
+        fi
+
+        if [[ -v dependency_map["$key"] && ${dependency_map["$key"]} ]]; then
+          echo "(2b)dependency_map[nothing]=${dependency_map["$key"]}"
+        fi
+
+        for key in "${!dependency_map[@]}"; do
+            echo "Key: $key"
+            echo "Value: ${dependency_map["$key"]}"
+        done
+
+        declare -p dependency_map
 
     done < <(generate_dependency_map)
 
@@ -62,12 +97,21 @@ process_affected_module(){
     affected_module_escaped=$(escape "$affected_module" )
     local affected_module_GAV_escaped=${folder_to_maven_map[$affected_module_escaped]}
     affected_module_GAV_escaped="${affected_module_GAV_escaped% }"
+
+    echo "affected_module_GAV=$(unescape $affected_module_GAV_escaped)"
+
     if [[ -v dependency_map["$affected_module_GAV_escaped"] && ${dependency_map[$affected_module_GAV_escaped]} ]]; then
+
+        echo "found entries in dependency_map - ${dependency_map[$affected_module_GAV_escaped]}"
+
         for dependent in ${dependency_map[$affected_module_GAV_escaped]}; do
             dependent_escaped=$(escape "$dependent")
             affected_modules_map["$dependent_escaped"]=$dependent_escaped
         done
     else
+
+        echo "didn't find entries in dependency_map, adding $(unescape $affected_module_GAV_escaped)"
+
         if [[ -n "$affected_module_GAV_escaped" ]]; then
             affected_modules_map["$affected_module_GAV_escaped"]=$affected_module_GAV_escaped
         fi
