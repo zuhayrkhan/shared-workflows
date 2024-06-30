d813997076b3587bd628869e40b00c2c43ec6f2d
Zuhayr Khan <ZuhayrKhan@mac.com>
2024-06-30 07:28:46 +0100

    Update BATS test path and clean up debug code
    The BATS test path in the GitHub workflow has been updated to use a more portable way of invoking the BATS runner. Additionally, unnecessary debug code in 'generate_dependency_map.sh' has been removed, including echo commands for displaying the current working directory and file list.

--- actions/determine-minimal-project-list/generate_dependency_map.sh
+++ actions/determine-minimal-project-list/generate_dependency_map.sh
@@ -2,9 +2,6 @@
 
 generate_dependency_map() {
 
-  echo "pwd:`pwd`"
-  echo "ls -latr:`ls -latr`"
-
   # get directory of current script
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
