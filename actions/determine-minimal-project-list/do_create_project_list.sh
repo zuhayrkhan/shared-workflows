95a7953d40aa5d89f6a499d41fc3974140704bc1
Zuhayr Khan <ZuhayrKhan@mac.com>
2024-07-01 05:42:54 +0100

    Remove unused bash scripts and commented code
    The commit deletes the generate_maven_map_file.sh script that was no longer in use. Furthermore, unnecessary commented code was removed from the do_create_project_list.sh file to improve code readability.

--- actions/determine-minimal-project-list/do_create_project_list.sh
+++ actions/determine-minimal-project-list/do_create_project_list.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-#declare -A assoc_array
-#assoc_array["known"]+="value_for_known "
-#
-#if [[ -v assoc_array["unknown"] ]]; then
-#     echo "Key 'unknown' is set."
-#else
-#     echo "Key 'unknown' is not set."
-#fi
-#
-#echo "Value for the key 'known': '${assoc_array["known"]}'"
-#echo "Value for the key 'unknown': '${assoc_array["unknown"]}'"
-#
-#exit 0;
-
 # get directory of current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
