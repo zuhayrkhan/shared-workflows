a436ba67dd7d54840e763f60d59a9d610f82e62f
Zuhayr Khan <ZuhayrKhan@mac.com>
2024-07-03 15:21:46 +0100

    Refactor project list creation and dependency mapping scripts
    The project list creation script and dependency mapping scripts have been greatly improved. The formatting and readability of the code are enhanced, array declarations are consistent, and functions contain more detailed inline comments. The changes also include the renaming of variables and functions to more meaningful names.

--- actions/determine-minimal-project-list/create_project_list.sh
+++ actions/determine-minimal-project-list/create_project_list.sh
@@ -1,101 +1,79 @@
 #!/usr/bin/env bash
 
-# get directory of current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# source the utility functions script
+# Source required scripts
 source "$SCRIPT_DIR/utility.sh"
-
 source "$SCRIPT_DIR/generate_dependency_map.sh"
 
-declare -gA maven_to_folder_map
-declare -gA folder_to_maven_map
-declare -gA affected_modules_map
-declare -gA project_list
-declare -gA dependency_map
+# Global associative arrays declarations
+declare -gA MAVEN_TO_FOLDER_MAP
+declare -gA FOLDER_TO_MAVEN_MAP
+declare -gA AFFECTED_MODULES_MAP
+declare -gA PROJECT_LIST
+declare -gA DEPENDENCY_MAP
 
-process_module_and_dir(){
-
+# process_module_and_dir(): Extracts the module and directory from a given string
+process_module_and_dir() {
     local mavenGAV_and_dir="$1"
-
     IFS="(" read -r mavenGAV dir <<< "$mavenGAV_and_dir"
     folder=${dir%)*}
-
-    maven_to_folder_map["$mavenGAV"]="$folder "
-    folder_to_maven_map["$folder"]="$mavenGAV "
-
+    MAVEN_TO_FOLDER_MAP["$mavenGAV"]="$folder "
+    FOLDER_TO_MAVEN_MAP["$folder"]="$mavenGAV "
     echo "$mavenGAV" "$folder"
-
 }
 
+# generate_and_handle_dependency_map(): Generates a map of dependencies between different modules
 generate_and_handle_dependency_map() {
-
     while IFS= read -r line; do
-
         IFS="|" read -r dependent dependency <<< "$line"
-
         read -r dependent_mavenGAV dependent_folder <<< "$(process_module_and_dir "$dependent")"
-
-        maven_to_folder_map["$dependent_mavenGAV"]="$dependent_folder "
-        folder_to_maven_map["$dependent_folder"]="$dependent_mavenGAV "
-
+        MAVEN_TO_FOLDER_MAP["$dependent_mavenGAV"]="$dependent_folder "
+        FOLDER_TO_MAVEN_MAP["$dependent_folder"]="$dependent_mavenGAV "
         read -r dependency_mavenGAV dependency_folder <<< "$(process_module_and_dir "$dependency")"
-
-        maven_to_folder_map["$dependency_mavenGAV"]="$dependency_folder "
-        folder_to_maven_map["$dependency_folder"]="$dependency_mavenGAV "
-
-        dependency_map["$dependency_mavenGAV"]+="$dependent_mavenGAV "
-
+        MAVEN_TO_FOLDER_MAP["$dependency_mavenGAV"]="$dependency_folder "
+        FOLDER_TO_MAVEN_MAP["$dependency_folder"]="$dependency_mavenGAV "
+        DEPENDENCY_MAP["$dependency_mavenGAV"]+="$dependent_mavenGAV "
     done < <(generate_dependency_map)
-
 }
 
 source "$SCRIPT_DIR/determine_changed_files.sh"
 
-process_affected_module(){
-
-    local affected_module
-    affected_module="$1"
-    local affected_module_GAV=${folder_to_maven_map[$affected_module]}
+# process_affected_module(): Process a given affected module by adding it and its dependents to the AFFECTED_MODULES_MAP
+process_affected_module() {
+    local affected_module="$1"
+    local affected_module_GAV=${FOLDER_TO_MAVEN_MAP[$affected_module]}
     affected_module_GAV="${affected_module_GAV% }"
-
-    if [[ -v dependency_map["$affected_module_GAV"] && ${dependency_map[$affected_module_GAV]} ]]; then
-        for dependent in ${dependency_map[$affected_module_GAV]}; do
-            affected_modules_map["$dependent"]=$dependent
+    if [[ -v DEPENDENCY_MAP["$affected_module_GAV"] && ${DEPENDENCY_MAP[$affected_module_GAV]} ]]; then
+        for dependent in ${DEPENDENCY_MAP[$affected_module_GAV]}; do
+            AFFECTED_MODULES_MAP["$dependent"]=$dependent
         done
-    else
-        if [[ -n "$affected_module_GAV" ]]; then
-            affected_modules_map["$affected_module_GAV"]=$affected_module_GAV
-        fi
+    elif [[ -n "$affected_module_GAV" ]]; then
+        AFFECTED_MODULES_MAP["$affected_module_GAV"]=$affected_module_GAV
     fi
-
 }
 
-determine_and_handle_changed_files(){
-
+# determine_and_handle_changed_files(): Identifies the changed files and handle the affected modules
+determine_and_handle_changed_files() {
     while IFS= read -r affected_module; do
       process_affected_module "$affected_module"
     done < <(determine_changed_files "$@")
-
 }
 
-list_affected_projects(){
-
-    for affected_module in "${affected_modules_map[@]}"; do
-        local affected_folder=${maven_to_folder_map[$affected_module]}
-        local affected_folder_trimmed
-        affected_folder_trimmed=$(trim "$affected_folder")
-        project_list["$affected_folder_trimmed"]=$affected_folder_trimmed
+# list_affected_projects(): Lists all the projects affected by the changes
+list_affected_projects() {
+    for affected_module in "${AFFECTED_MODULES_MAP[@]}"; do
+        local affected_folder=${MAVEN_TO_FOLDER_MAP[$affected_module]}
+        local affected_folder_trimmed=$(trim "$affected_folder")
+        PROJECT_LIST["$affected_folder_trimmed"]=$affected_folder_trimmed
     done
-
-    echo "project_list=$(echo "${project_list[@]}" | sed 's|/./||g' | tr ' ' ',' )" | tee -a $GITHUB_OUTPUT
-
+    # Output the affected projects list
+    echo "project_list=$(echo "${PROJECT_LIST[@]}" | sed 's|/./||g' | tr ' ' ',' )" | tee -a $GITHUB_OUTPUT
 }
 
-create_project_list(){
-
+# create_project_list(): The main function that ties all the other functions together
+create_project_list() {
     generate_and_handle_dependency_map
     determine_and_handle_changed_files "$@"
     list_affected_projects
-
 }
--- actions/determine-minimal-project-list/determine_changed_files.sh
+++ actions/determine-minimal-project-list/determine_changed_files.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
+# determine_changed_files(): Determines which files have changed
+# Takes a list of changed files as the input
 determine_changed_files() {
+    # Set Internal Field Separator (IFS) to new line character
+    IFS=$'\n'
 
-  IFS=$'\n'
-  module_paths=($(find . -name "pom.xml" | sed 's|/[^/]*$||' | sed 's| $||' | awk '{ printf "%d %s/\n", length, $0 }' | sort -nr | cut -d" " -f2-))
+    # Find paths for all "pom.xml" files
+    # The paths are then sorted by length in descending order
+    # This allows us to match the more specific paths before the less specific ones
+    module_paths=($(find . -name "pom.xml" | sed 's|/[^/]*$||' | sed 's| $||' | awk '{ printf "%d %s/\n", length, $0 }' | sort -nr | cut -d" " -f2-))
 
-  IFS=' '
-  changed_files=("$@")
+    # Reset IFS back to its default
+    IFS=' '
 
-  for file in "${changed_files[@]}"; do
-      prefixed_file=./$file
-      for module_path in "${module_paths[@]}"; do
-          # Check if the file path starts with the module path
-          if [[ "$module_path" != "./" ]]; then
-            match_regex="^${module_path//\//\\/}"
-            if [[ "$prefixed_file" =~ $match_regex ]]; then
-                echo "$module_path"
-                break # Break after the first match to avoid checking less specific paths
+    # Parse the input arguments as changed files
+    changed_files=("$@")
+
+    # Iterate over changed files
+    for file in "${changed_files[@]}"; do
+        # Prefix the file with "./" to make it compatible with the module_paths
+        prefixed_file=./$file
+
+        # Check each changed file against every module path
+        # If the file path starts with the module path, it is part of the module
+        for module_path in "${module_paths[@]}"; do
+            # Ignore the root directory
+            if [[ "$module_path" != "./" ]]; then
+                # Create a regex for matching
+                match_regex="^${module_path//\//\\/}"
+                # If the prefixed_file starts with module_path
+                if [[ "$prefixed_file" =~ $match_regex ]]; then
+                    echo "$module_path"
+                    break # Exit the loop after the first match to avoid matching less specific paths
+                fi
             fi
-          fi
-      done
-  done
-
+        done
+    done
 }
-
-#determine_changed_files "$@";
